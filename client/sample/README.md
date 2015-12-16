8bbcda57aee05a7d2164358ca348526739d32f3a
tyage <namatyage@gmail.com>
2015-12-16 14:28:40 +0900

    comment about assignment algorithm

--- client/sample/README.md
+++ client/sample/README.md
@@ -1,3 +1,11 @@
 # Sample task assignment program
 
 Assign a task using Task pooler.
+
+Default assignment algorithm is random assignment.
+
+Run with:
+
+```sh
+$ AWS_ACCESS_KEY_ID=XXX AWS_SECRET_ACCESS_KEY=XXX npm start
+```
--- client/sample/task.js
+++ client/sample/task.js
@@ -11,8 +11,10 @@ const mturk = new MTurk({
   awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
+// assignment algorithm should be compiled because it will be run by worker's browser
 const algorithm = fs.readFileSync('./assignment-dist/random.js');
-const budget = 0.06;
+
+const budget = 0.04;
 const hitCost = 0.02;
 for (let i = 0; i < budget / hitCost; ++i) {
   mturk.createHIT(algorithm, taskSetId, {
