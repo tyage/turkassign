e3eba22d2eae78d360aa8d767fedf058ed65a47e
tyage <namatyage@gmail.com>
2015-12-16 14:52:45 +0900

    put tasks to taskPooler

--- client/src/task-pooler.js
+++ client/src/task-pooler.js
@@ -1,9 +1,13 @@
-import https from 'https';
+import fetch from 'fetch';
 import config from './config';
 
 class TaskPooler {
   setTasks(tasks) {
     const taskPoolerAddress = config.get('taskPoolerAddress');
+    return fetch(taskPoolerAddress, {
+      method: 'PUT',
+      body: JSON.stringify(tasks)
+    });
   }
 }
 
