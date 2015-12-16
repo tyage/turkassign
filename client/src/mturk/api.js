0532b75d74029fbd00a69e3227185c6cb7bf26e5
tyage <namatyage@gmail.com>
2015-12-16 14:51:10 +0900

    node-fetch returning

--- client/src/mturk/api.js
+++ client/src/mturk/api.js
@@ -1,33 +1,12 @@
-import https from 'https';
 import crypto from 'crypto';
+import fetch from 'node-fetch';
 import { loadXML } from './parser';
 
 const generateHmac = (data, key) => {
   return crypto.createHmac('sha1', key).update(data).digest('base64');
 };
 
-const fetch = options => {
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, res => {
-      let data = '';
-      res.on('data', chunk => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        resolve(data);
-      });
-    });
-
-    req.on('error', function(e) {
-      console.error(e);
-      reject(e);
-    });
-
-    req.end();
-  });
-};
-
-const apiEndpoint = 'mechanicalturk.sandbox.amazonaws.com';
+const apiEndpoint = 'https://mechanicalturk.sandbox.amazonaws.com';
 const apiVersion = '2014-08-15';
 const apiService = 'AWSMechanicalTurkRequester';
 
@@ -55,17 +34,13 @@ class API {
       this.api.awsSecretAccessKey);
 
     const param = Object.keys(params).map((k) => `${k}=${encodeURIComponent(params[k])}`).join('&');
-    const options = {
-      hostname: this.api.endpoint,
-      port: 443,
-      path: `/?{param}`,
-      method: 'GET'
-    };
 
-    return fetch(options).then(text => {
-      console.log(text); // TODO: use general logger
-      return loadXML(text);
-    });
+    return fetch(`${this.api.endpoint}/?${param}`)
+      .then(res => res.text())
+      .then(text => {
+        console.log(text); // TODO: use general logger
+        return loadXML(text);
+      });
   }
 
   createHIT(params) {
