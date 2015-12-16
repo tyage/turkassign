cb25443179f08a15e7b01731d7ebd3ed7b205ca8
tyage <namatyage@gmail.com>
2015-12-16 14:10:35 +0900

    random algorithm

--- client/sample/random.js
+++ client/sample/random.js
@@ -1,4 +1,20 @@
-function(worker, taskSet) {
-  // worker assigned a task which is choosed by algorithm
-  // e.g. 乱択アルゴリズム
-};
+// worker assigned a task which is choosed by random
+//
+// worker: {
+//   id: worker id
+// }
+//
+// taskSet: [
+//   {
+//     data: task data,
+//     budget: remaining budget, // budget should be > 0
+//     reserve: reserve this task,
+//     unreserve: unreserve this task
+//   }
+// ]
+
+const taskId = parseInt(Math.random() * taskSet.length);
+const selectedTask = taskSet[taskId];
+selectedTask.reserve();
+
+document.getElementById('content').innerHTML = selectedTask.data.html;
