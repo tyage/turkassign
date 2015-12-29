1d50b7e04d354aef64744eeb5e461d1317dac149
tyage <namatyage@gmail.com>
2015-12-29 17:43:43 +0900

    若い人、普通の人グループでわける

--- client/sample/tasks.js
+++ client/sample/tasks.js
@@ -1,16 +1,18 @@
 const tasks = [
   {
-    image: {
-      url: '56_Christopher_McDonald_0002.jpg',
-      id: 2
-    },
+    images: [
+      { url: '56_Christopher_McDonald_0002.jpg', id: 1 },
+      { url: '56_Christopher_McDonald_0002.jpg', id: 2 }
+    ],
+    age: 'young',
     budget: 2
   },
   {
-    image: {
-      url: '36_Yunjin_Kim_0008.jpg',
-      id: 2
-    },
+    images: [
+      { url: '36_Yunjin_Kim_0008.jpg', id: 3 },
+      { url: '36_Yunjin_Kim_0008.jpg', id: 4 },
+    ],
+    age: 'middle',
     budget: 2
   }
 ];
