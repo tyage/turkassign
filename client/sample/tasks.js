7056429991fa263f5ef7db81abca61c6ba67ec40
tyage <namatyage@gmail.com>
2015-12-29 17:17:15 +0900

    images

--- client/sample/tasks.js
+++ client/sample/tasks.js
@@ -1,10 +1,10 @@
 const tasks = [
   {
-    images: [
-      { id: 1, url: '56_Christopher_McDonald_0002.jpg' },
-      { id: 2, url: '36_Yunjin_Kim_0008.jpg' }
-    ],
-    gender: 'man',
+    image: '56_Christopher_McDonald_0002.jpg',
+    budget: 2
+  },
+  {
+    image: '36_Yunjin_Kim_0008.jpg',
     budget: 2
   }
 ];
