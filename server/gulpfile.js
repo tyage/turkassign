999a68c083130b2f7305e9a84ecc49b8bd6e8c0d
tyage <namatyage@gmail.com>
2015-12-29 16:50:38 +0900

    build public src

---
+++ server/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack-stream');
+var webpackConfig = require('./webpack.config.js');
+
+var config = {
+  src: 'public/src',
+  dist: 'public/dist'
+};
+
+gulp.task('js', function() {
+  return gulp.src('')
+    .pipe(webpack(webpackConfig))
+    .on('error', function(e) {
+      gutil.log(gutil.colors.red(e));
+      this.emit('end');
+    })
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(config.src + '/js/**', ['js']);
+});
+
+gulp.task('default', ['js'], function() {
+});
