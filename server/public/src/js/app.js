093a8f3fbc786d6e537f52a2370ae9515756b42e
tyage <namatyage@gmail.com>
2015-12-29 17:31:44 +0900

    api return taskSet

--- server/public/src/js/app.js
+++ server/public/src/js/app.js
@@ -3,7 +3,7 @@ import Task from './task';
 
 window.fetchTaskSet = () => {
   const path = `${taskPoolerAddress}/list/${taskSetId}`;
-  return $.getJSON(path).then(({ rawTaskSet }) => {
+  return $.getJSON(path).then(({ taskSet: rawTaskSet }) => {
     const taskSet = [];
     for (let i = 0, l = rawTaskSet.length; i < l; ++i) {
       taskSet.push(new Task(rawTaskSet[i]));
