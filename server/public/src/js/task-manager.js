37155af12d12a6fe5cf786962036921a31b04ddd
tyage <namatyage@gmail.com>
2015-12-30 18:49:34 +0900

    ページから離脱する場合に確保したtaskを返却する

--- server/public/src/js/task-manager.js
+++ server/public/src/js/task-manager.js
@@ -2,12 +2,28 @@ import $ from 'jquery';
 import _ from 'lodash';
 import Task from './task';
 
+const tasks = [];
+const registerNewTask = task => {
+  tasks.push(task);
+};
+
+// ページから離脱する場合に確保したtaskを返却する
+const unreserveTasks = () => {
+  _.each(tasks, task => {
+    if (task.isReserved) {
+      task.unreserve();
+    }
+  });
+};
+$.on('onbeforeunload', unreserveTasks);
+
 const fetchTaskSet = taskSetId => {
   // XXX: taskPoolerAddressは埋め込まれたaddressから取ってくる
   const path = `${window.taskPoolerAddress}/list/${taskSetId}`;
   return $.getJSON(path).then(({ taskSet }) => {
     return _.map(taskSet, taskData => {
       const task = new Task(taskData, taskSetId, i);
+      registerNewTask(task);
       return task;
     });
   });
@@ -21,6 +37,8 @@ const fetchAvailabeleTaskSet = taskSetId => {
 };
 
 export default {
+  registerNewTask,
+  unreserveTasks,
   fetchAvailabeleTaskSet,
   fetchTaskSet
 };
