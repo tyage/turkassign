0ca843807b423a833957b7e2e56450fe1eef998f
tyage <namatyage@gmail.com>
2015-12-30 18:35:48 +0900

    fetchTaskSet function moved to task-manager

--- server/public/src/js/app.js
+++ server/public/src/js/app.js
@@ -1,29 +1,5 @@
-import $ from 'jquery';
-import Task from './task';
+import { fetchAvailabeleTaskSet, fetchTaskSet } from './task-manager';
 
-window.fetchTaskSet = (taskSetId) => {
-  // XXX: taskPoolerAddressは埋め込まれたaddressから取ってくる
-  const path = `${window.taskPoolerAddress}/list/${taskSetId}`;
-  return $.getJSON(path).then(({ taskSet: rawTaskSet }) => {
-    const taskSet = [];
-    for (let i = 0, l = rawTaskSet.length; i < l; ++i) {
-      const task = new Task(rawTaskSet[i], taskSetId, i);
-      taskSet.push(task);
-    }
-    return taskSet;
-  });
-};
-
-// task which budget > 0
-window.fetchAvailabeleTaskSet = (taskSetId) => {
-  return window.fetchTaskSet(taskSetId).then(taskSet => {
-    const filteredTaskSet = [];
-    for (let i = 0, l = taskSet.length; i < l; ++i) {
-      const task = taskSet[i];
-      if (task.budget > 0) {
-        filteredTaskSet.push(task);
-      }
-    }
-    return filteredTaskSet;
-  });
-};
+// expose task manager
+window.fetchAvailabeleTaskSet = fetchAvailabeleTaskSet;
+window.fetchTaskSet = fetchTaskSet;
---
+++ server/public/src/js/task-manager.js
@@ -0,0 +1,34 @@
+import $ from 'jquery';
+import Task from './task';
+
+fetchTaskSet = (taskSetId) => {
+  // XXX: taskPoolerAddressは埋め込まれたaddressから取ってくる
+  const path = `${window.taskPoolerAddress}/list/${taskSetId}`;
+  return $.getJSON(path).then(({ taskSet: rawTaskSet }) => {
+    const taskSet = [];
+    for (let i = 0, l = rawTaskSet.length; i < l; ++i) {
+      const task = new Task(rawTaskSet[i], taskSetId, i);
+      taskSet.push(task);
+    }
+    return taskSet;
+  });
+};
+
+// task which budget > 0
+fetchAvailabeleTaskSet = (taskSetId) => {
+  return window.fetchTaskSet(taskSetId).then(taskSet => {
+    const filteredTaskSet = [];
+    for (let i = 0, l = taskSet.length; i < l; ++i) {
+      const task = taskSet[i];
+      if (task.budget > 0) {
+        filteredTaskSet.push(task);
+      }
+    }
+    return filteredTaskSet;
+  });
+};
+
+export default {
+  fetchAvailabeleTaskSet,
+  fetchTaskSet
+};
