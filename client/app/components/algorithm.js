728df888e5a826c409ddac4db6110fd923a11961
tyage <namatyage@gmail.com>
2016-02-01 04:32:14 +0900

    save algorithm

--- client/app/components/algorithm.js
+++ client/app/components/algorithm.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import ConfigService from '../services/config';
 
 export default class Algorithm extends React.Component {
   constructor(props) {
     super(props);
+  }
+  onChange(e) {
+    ConfigService.set('algorithm', e.target.value);
   }
   render() {
     return (
       <div>
         <div className="form-section">
           <label>Assignment Algorithm</label>
-          <textarea rows="30" cols="80" disabled={ this.props.disabled }></textarea>
+          <textarea rows="30" cols="80"
+            disabled={ this.props.disabled }
+            onChange={ this.onChange }
+            defaultValue={ ConfigService.get('algorithm') }
+            ></textarea>
         </div>
       </div>
     );
